eaa800beb38f8bf4775b2272f59c6415621eed0d
Filip Wagner <filipwagner23@gmail.com>
2022-03-27 20:34:42 +0200

    many changes again

--- src/app/components/page-display-modal/page-display-modal.component.ts
+++ src/app/components/page-display-modal/page-display-modal.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { Pages } from 'src/app/models/pages';
+import { DataExchangeService } from 'src/app/services/data-exchange.service';
 
 @Component({
   selector: 'app-page-display-modal',
@@ -7,10 +9,17 @@ import { faFile } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./page-display-modal.component.scss']
 })
 export class PageDisplayModalComponent implements OnInit {
-
-  constructor() { }
+  showModal:boolean=false;
+  @Input() pages_iterator:number;
+  pages_to_show:any=[];
+  constructor(private exchange:DataExchangeService) { }
   faFile=faFile;
   ngOnInit(): void {
+    this.pages_to_show=this.exchange.get_result_pages()[this.pages_iterator];
+  }
+  setModal(){
+    console.log(this.pages_to_show);
+    this.showModal=true;
   }
 
 }
